bf38730a627a6c7a9c5a4b66d4004b01d9073673
Anwesh43 <anweshthecool0@gmail.com>
2019-01-15 20:41:01 +0530

    adding libraries and adding constants

--- slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
+++ slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.slabuprectview
 /**
  * Created by anweshmishra on 15/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val slabs : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
