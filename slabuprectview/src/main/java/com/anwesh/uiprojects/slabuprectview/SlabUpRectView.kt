ff9ade8b2e666e2d353792f13f532254ef661d12
Anwesh43 <anweshthecool0@gmail.com>
2019-01-15 21:15:54 +0530

    adding SlabUpRectView class

--- slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
+++ slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawSURNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SlabUpRectView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
