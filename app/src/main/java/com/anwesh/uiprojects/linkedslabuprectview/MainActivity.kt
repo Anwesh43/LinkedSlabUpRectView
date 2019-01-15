f610e88f31179cf9adf77d733056be62c89d1c18
Anwesh43 <anweshthecool0@gmail.com>
2019-01-15 22:24:10 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedslabuprectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedslabuprectview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedslabuprectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.slabuprectview.SlabUpRectView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SlabUpRectView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
