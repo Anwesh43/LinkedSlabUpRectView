910020b9e58a6149224da86be7e403a4e244dcb0
Anwesh43 <anweshthecool0@gmail.com>
2019-01-15 21:00:28 +0530

    adding drawSURNode canvas extension method

--- slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
+++ slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
@@ -27,3 +27,26 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawSURNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    val size : Float = gap / sizeFactor
+    val xGap : Float = size / (2 * slabs)
+    save()
+    translate(gap * (i + 1), h/2 + size/2)
+    rotate(90f * sc2)
+    translate(-size/4, 0f)
+    for (j in 0..(slabs - 1)) {
+        val sc : Float = sc1.divideScale(j, slabs)
+        save()
+        translate(xGap * j, 0f)
+        drawRect(RectF(0f, -size * sc, xGap, 0f), paint)
+        restore()
+    }
+    restore()
+}
