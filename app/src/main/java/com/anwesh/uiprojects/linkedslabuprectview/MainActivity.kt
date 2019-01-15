794a3e7530975650f95bd40c8ba8f55ea4aca571
Anwesh43 <anweshthecool0@gmail.com>
2019-01-15 22:23:15 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedslabuprectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedslabuprectview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedslabuprectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.slabuprectview.SlabUpRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SlabUpRectView.create(this)
     }
 }
