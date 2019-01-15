00d679466ef8844bc992b69040098a75a526c1c3
Anwesh43 <anweshthecool0@gmail.com>
2019-01-15 21:49:21 +0530

    adding SURNode class

--- slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
+++ slabuprectview/src/main/java/com/anwesh/uiprojects/slabuprectview/SlabUpRectView.kt
@@ -115,4 +115,52 @@ class SlabUpRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SURNode(var i : Int, val state : State = State()) {
+
+        private var next : SURNode? = null
+        private var prev : SURNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SURNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSURNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SURNode {
+            var curr : SURNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+
+    }
 }
